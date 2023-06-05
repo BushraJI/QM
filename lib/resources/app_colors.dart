6139a3df7cb277f632b6ded3fc2423c8bf13adbd
abdullah <muhammad.abdullah@xorbix.com>
2023-06-06 01:34:45 +0500

    Add utils files

---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,50 @@
+import 'dart:ui';
+
+class AppColors {
+  // Fill Colors
+  static const Color upperTitleColor = Color(0XFF759CA8);
+  static const Color lowerTitleColor = Color(0XFF0C343F);
+  static const Color blueButtonColor = Color(0XFF9CC1CC);
+  static const Color goldButtonColor = Color(0XFFC6A97D);
+  static const Color primary = Color(0xffD70F64);
+  static const Color backgroundColor = Color(0xffeef1f6);
+
+  // Grey colors
+  static const Color textFieldInputColor = Color(0XFF35425B);
+  static const Color textFieldTitleColor = Color(0XFF72809D);
+  static const Color textFieldBorderColor = Color(0XFF97B0C3);
+  static const Color searchFieldBorderColor = Color(0XFFE7ECF0);
+  static const Color searchFieldHintTextColor = Color(0XFF848EA3);
+  static const Color greyButtonFillColor = Color(0XFFD7E1EA);
+  static const Color greyToggleButtonColor = Color(0XFFF0F4F8);
+  static const Color blackButtonTextColor = Color(0XFFFAFBFD);
+  static const Color homeTextColor = Color(0xFF0E172C);
+
+  static const Color grayBackgroundColor = Color(0xFFFAFAFB);
+  static const Color dividerBackgroundColor = Color(0x41DDDDDD);
+  static const Color disableFillColor = Color(0xFFF7FAFC);
+  static const Color disableBorderColor =   Color(0xFFDCE1E7);
+  static const Color activeFillColor = Color(0x2683C5BE);
+  static const Color activeBorderColor = Color(0xFF9CC1CC);
+  static const Color selectedFillColor = Color(0xFFC6A97D);
+  static const Color selectedBorderColor =   Color(0xFFC6A97D);
+
+  static const Color textTitleColor = Color(0xFF35425B);
+  static const Color textDetailColor = Color(0xFF72809D);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF35425B);
+  static const Color selectedNavigationItemColor = Color(0xFF0C343F);
+  static const Color unSelectedNavigationItemColor = Color(0x800C343F);
+
+  static const Color lightIconColor = Color(0xFF5D6275);
+
+  // Account List Item Colors for Select Business Screen
+  static const accountListItemColors = <Color>[
+    Color(0XFFC2ACFA),
+    Color(0XFFE29578),
+    Color(0XFF78D5E2),
+    Color(0XFFE2B878),
+    Color(0XFFE27E78),
+    Color(0XFF78DCE2),
+  ];
+}
---
+++ lib/resources/app_constants.dart
@@ -0,0 +1,11 @@
+
+class AppConstants {
+  static const keyToken = 'key_token';
+  static const keyNewsFeedCategories = 'key_news_feed_categories';
+  static const fcmToken = 'key_fcm_token';
+  static const favoriteBlogPostIds = 'key_favorite_blog_post_ids';
+  static const keyIsAllCategories = 'key_is_all_categories';
+  static const keySelectedCategoryList = 'key_selected_categories_list';
+  static const accountId = 'accountId';
+
+}
