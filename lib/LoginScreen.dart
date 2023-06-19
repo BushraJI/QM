122597dd1580b6394ea2f667e72ce41d12924b10
BushraJI <bjierte76@gmail.com>
2023-06-20 04:26:32 +0500

    login with phnumber with bg white

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -35,147 +35,154 @@ class LoginScreenState extends State<LoginScreen> {
             ),
             SingleChildScrollView(
               child: Positioned(
-                  child: Padding(
-                padding: const EdgeInsets.only(top: 70, right: 20, left: 20),
-                child: Column(
-                  children: [
-                    Image.asset('assets/images/panda.png',
-                        height: 100, width: 100),
-                    const SizedBox(height: 20),
-                    const Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Welcome to your restaurant',
-                        style: TextStyle(fontSize: 25, color: Colors.black),
-                      ),
-                    ),
-                    const SizedBox(height: 5),
-                    const Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'portal from foodpanda',
-                        style: TextStyle(fontSize: 23, color: Colors.black),
-                      ),
-                    ),
-                    const SizedBox(height: 40),
-                    const Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Login to your account',
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.blueGrey,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    const SizedBox(height: 50),
-                    CustomTextField('Email'),
-                    const SizedBox(height: 30),
-                    CustomTextField('Password'),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    ButtonDesign(
-                      buttonText: "Login",
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => HomeScreen()),
-                        );
-                      },
-                    ),
-                    const SizedBox(
-                      height: 35,
-                    ),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ForgetPasswordScreen()),
-                        );
-                      },
-                      child: Align(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 70, right: 20, left: 20),
+                  child: Column(
+                    children: [
+                      Image.asset('assets/images/panda.png',
+                          height: 100, width: 100),
+                      const SizedBox(height: 20),
+                      const Align(
                         alignment: Alignment.center,
                         child: Text(
-                          'Forgot password?',
-                          style: TextStyle(
-                            color: Colors.pink,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
+                          'Welcome to your restaurant',
+                          style: TextStyle(fontSize: 25, color: Colors.black),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                    Row(children: <Widget>[
-                      Expanded(
-                        child: new Container(
-                            margin:
-                                const EdgeInsets.only(left: 10.0, right: 15.0),
-                            child: Divider(
-                              indent: 10,
-                              color: Colors.black,
-                              height: 50,
-                            )),
-                      ),
-                      Text("OR"),
-                      Expanded(
-                        child: new Container(
-                            margin:
-                                const EdgeInsets.only(left: 15.0, right: 10.0),
-                            child: Divider(
-                              endIndent: 10,
-                              color: Colors.black,
-                              height: 50,
-                            )),
-                      ),
-                      const SizedBox(height: 20),
-                    ]),
-                    ButtonDesign(
-                      buttonText: "Signup",
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SignupScreen()),
-                        );
-                      },
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Align(
+                      const SizedBox(height: 5),
+                      const Align(
                         alignment: Alignment.center,
-                        child: InkWell(
-                          onTap: () {},
-                          child: TextButton(
-                            onPressed: () {},
-                            child: Row(
-                              children: [
-                                IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Icons.call,
-                                      color: Colors.pink,
-                                      size: 23,
-                                    )),
-                                const SizedBox(width: 5),
-                                Text(
-                                  'Login with phone number',
-                                  style: TextStyle(
-                                      fontSize: 22,
-                                      color: Colors.pink,
-                                      fontWeight: FontWeight.bold),
-                                )
-                              ],
+                        child: Text(
+                          'portal from foodpanda',
+                          style: TextStyle(fontSize: 23, color: Colors.black),
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      const Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Login to your account',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.blueGrey,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const SizedBox(height: 50),
+                      CustomTextField('Email'),
+                      const SizedBox(height: 30),
+                      CustomTextField('Password'),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      ButtonDesign(
+                        buttonText: "Login",
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeScreen()),
+                          );
+                        },
+                      ),
+                      const SizedBox(
+                        height: 35,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ForgetPasswordScreen()),
+                          );
+                        },
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Forgot password?',
+                            style: TextStyle(
+                              color: Colors.pink,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                         ),
                       ),
-                    )
-                  ],
+                      const SizedBox(height: 20),
+                      Row(children: <Widget>[
+                        Expanded(
+                          child: new Container(
+                              margin: const EdgeInsets.only(
+                                  left: 10.0, right: 15.0),
+                              child: Divider(
+                                indent: 10,
+                                color: Colors.black,
+                                height: 50,
+                              )),
+                        ),
+                        Text("OR"),
+                        Expanded(
+                          child: new Container(
+                              margin: const EdgeInsets.only(
+                                  left: 15.0, right: 10.0),
+                              child: Divider(
+                                endIndent: 10,
+                                color: Colors.black,
+                                height: 50,
+                              )),
+                        ),
+                        const SizedBox(height: 20),
+                      ]),
+                      ButtonDesign(
+                        buttonText: "Signup",
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignupScreen()),
+                          );
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: InkWell(
+                            onTap: () {},
+                            child: Container(
+                              color: Colors.white,
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Row(
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                        Icons.call,
+                                        color: Colors.pink,
+                                        size: 23,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 5),
+                                    Text(
+                                      'Login with phone number',
+                                      style: TextStyle(
+                                          fontSize: 22,
+                                          color: Colors.pink,
+                                          fontWeight: FontWeight.bold),
+                                      //],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              )),
+              ),
             ),
           ],
         ),
